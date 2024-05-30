7ad3f8c19c6e4edd22a4aa38c5dd60a7642a8dde
MartaBlancobe <martablancobe@gmail.com>
2024-05-30 17:27:02 +0200

    Se añaden cambios importantes en el diseño del formulario de contacto

--- src/app/components/oil-changes/oil-changes.component.ts
+++ src/app/components/oil-changes/oil-changes.component.ts
@@ -1,9 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-oil-changes',
   standalone: true,
-  imports: [],
+  imports: [ButtonModule,CommonModule ],
   templateUrl: './oil-changes.component.html',
   styleUrl: './oil-changes.component.scss'
 })
